afce240ce9626f7dad068da7d0abd609972d8655
Smiley~ <45153558+davekgw@users.noreply.github.com>
2025-08-28 12:08:35 +0800

    Update exercise#1.js

--- pertemuan-5/exercise#1.js
+++ pertemuan-5/exercise#1.js
@@ -1,10 +1,23 @@
-// Buatlah program untuk menghitung BMI dengan menggunakan
-// IIFE % Callback function
+//Buatlah program untuk menghitung BMI dengan menggunakan 
+//IIFE & Callback function 
+//dengan parameter dan return value
 
-// 1. IIFE
-(function(berat, tinggi){
-    let bmi = berat / (tinggi * tinggi);
-    console.log("BMI anda adalah: " + bmi.toFixed(2));
-})(70, 1.70)
+//Make by Dave Koagow
 
-//2. Callback
+(function(){
+    function calculateBMI(berat, tinggi, callback){
+        return callback(berat, tinggi);
+    }
+
+    function bmi(berat, tinggi){
+        let tinggiMeter = tinggi / 100;
+        let bmi = berat / (tinggiMeter * tinggiMeter);
+        return bmi.toFixed(2);
+    }
+
+    let berat = 70;
+    let tinggi = 175;
+
+    let res = calculateBMI(berat, tinggi, bmi);
+    console.log("Nilai BMI anda adalah: " + res);
+})();
